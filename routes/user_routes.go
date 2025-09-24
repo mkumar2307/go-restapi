08445005627c63ec1b563c992f60c66651476240
Manoj Kumar Gudipati <manojgudipati@hotmail.com>
2025-09-24 13:21:59 +0200

    Adding CRUS routes for users

---
+++ routes/user_routes.go
@@ -0,0 +1,18 @@
+package routes
+
+import (
+	"myapp/controllers"
+
+	"github.com/gin-gonic/gin"
+)
+
+func RegisterUserRoutes(router *gin.Engine) {
+	userRoutes := router.Group("/users")
+	{
+		userRoutes.GET("/", controllers.GetUsers)         // List all users
+		userRoutes.POST("/", controllers.CreateUser)      // Create new user
+		userRoutes.GET("/:id", controllers.GetUser)       // Get single user
+		userRoutes.PUT("/:id", controllers.UpdateUser)    // Update user
+		userRoutes.DELETE("/:id", controllers.DeleteUser) // Delete user
+	}
+}
